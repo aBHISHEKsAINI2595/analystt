3a7a328fcac05bdf8f679d94ad6be8995b3c3527
aBHISHEKsAINI2595 <116787785+aBHISHEKsAINI2595@users.noreply.github.com>
2023-02-03 19:37:39 +0530

    Update part 2.py

--- part 2.py
+++ part 2.py
@@ -13,42 +13,59 @@ link_list=[]
 description_list=[]
 asin_list=[]
 manufacturer_list=[]
-def get_info():
+
+def get_link_list():
     links=driver.find_elements(By.CSS_SELECTOR, 'a[class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]')
     for link in links:
         link_list.append(link.get_attribute('href'))
-    for ll in link_list[:200]:
-        # print(ll)
-        driver.get(ll)
-        # driver.fullscreen_window()
-        description = driver.find_element(By.ID, 'feature-bullets')
-        desc_info=(description.text)
-        description_list.append(desc_info)
+        # print(link_list)
+get_link_list()
+while len(link_list) <= 200:
+    next_page = driver.find_element(By.CSS_SELECTOR, 'a[class="s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"]')
+    driver.get(next_page.get_attribute('href'))
+    get_link_list()
+
+# print(len(link_list))
+# print(link_list)
+
+def get_info():
+    for ll in link_list:
         try:
-            asin = driver.find_element(By.XPATH, '//*[@id="detailBullets_feature_div"]/ul/li[4]')
-            asin_info=(asin.text)
-            # print(asin_info)
-            asin_list.append(asin_info)
-            # print("asin try")
+            # print(ll)
+            driver.get(ll)
+            # driver.fullscreen_window()
+            description = driver.find_element(By.ID, 'feature-bullets')
+            desc_info=(description.text)
+            description_list.append(desc_info)
             try:
-                manufacturer_name = driver.find_element(By.XPATH, '//*[@id="detailBullets_feature_div"]/ul/li[8]')
-                # print(manufacturer_name.text)
-                # print("manu try")
+                asin = driver.find_element(By.XPATH, '//*[@id="detailBullets_feature_div"]/ul/li[4]')
+                asin_info=(asin.text)
+                # print(asin_info)
+                asin_list.append(asin_info)
+                # print("asin try")
+                try:
+                    manufacturer_name = driver.find_element(By.XPATH, '//*[@id="detailBullets_feature_div"]/ul/li[8]')
+                    manufacturer_info = (manufacturer_name.text)
+                    # print(manufacturer_info)
+                    manufacturer_list.append(manufacturer_info)
+                except:
+                    manufacturer_name = driver.find_element(By.XPATH, '// *[ @ id = "detailBullets_feature_div"] / ul / li[3]')
+                    manufacturer_info = (manufacturer_name.text)
+                    # print(manufacturer_info)
+                    manufacturer_list.append(manufacturer_info)
             except:
-                manufacturer_name = driver.find_element(By.XPATH, '// *[ @ id = "detailBullets_feature_div"] / ul / li[3]')
-                # print(manufacturer_name.text)
-                # print("manu except")
+                asin = driver.find_element(By.XPATH, '// *[ @ id = "productDetails_detailBullets_sections1"] / tbody / tr[1]')
+                asin_info = (asin.text)
+                # print(asin_info)
+                asin_list.append(asin_info)
+                manufacturer_name = driver.find_element(By.XPATH, '//*[@id="productDetails_techSpec_section_1"]/tbody/tr[2]')
+                manufacturer_info=(manufacturer_name.text)
+                # print(manufacturer_info)
+                manufacturer_list.append(manufacturer_info)
+            finally:
+                # time.sleep(2)
+                driver.back()
         except:
-            asin = driver.find_element(By.XPATH, '// *[ @ id = "productDetails_detailBullets_sections1"] / tbody / tr[1]')
-            asin_info = (asin.text)
-            # print(asin_info)
-            asin_list.append(asin_info)
-            manufacturer_name = driver.find_element(By.XPATH, '//*[@id="productDetails_techSpec_section_1"]/tbody/tr[2]')
-            manufacturer_info=(manufacturer_name.text)
-            # print(manufacturer_info)
-            manufacturer_list.append(manufacturer_info)
-        finally:
-            # time.sleep(2)
             driver.back()
 
 while("" in link_list):
@@ -56,10 +73,7 @@ while("" in link_list):
 
 
 get_info()
-while True:
-    next_page = driver.find_element(By.XPATH, '// *[ @ id = "search"] / div[1] / div[1] / div / span[1] / div[1] / div[32] / div / div / span / a[3]')
-    driver.get(next_page.get_attribute('href'))
-    get_info()
+
 
 df1 = pd.DataFrame({"Description": description_list})
 df2 = pd.DataFrame({"ASIN":asin_list})
